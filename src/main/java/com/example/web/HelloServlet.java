68457396e4df5445428b36501b61f6a464776cc2
xvido <xvido@stuba.sk>
2023-06-15 15:47:57 +0200

    Updated visual

--- src/main/java/com/example/web/HelloServlet.java
+++ src/main/java/com/example/web/HelloServlet.java
@@ -9,25 +9,31 @@ import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
-
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-
+    Connection connection = null;
+    ResultSet resultSet;
+    public void init() throws ServletException{
         String jdbcURL = "jdbc:postgresql://localhost:5432/Skuska";
         String username = "postgres";
         String password = "123";
-
-        try  {
-            Connection connection = null;
+        try {
             Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(jdbcURL,username,password);
+            connection = DriverManager.getConnection(jdbcURL, username, password);
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM cypherauditlog");
-
+            resultSet = statement.executeQuery("SELECT * FROM cypherauditlog");
+        }
+        catch (SQLException e) {
+            throw new ServletException("Database connection failed",e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        try  {
             PrintWriter out = response.getWriter();
             out.println("<html><body>");
-            out.println("<table>");
-            out.println("<tr><th>ID</th><th>Input</th><th>Output</th><th>Cypher</th><th>Timestamp</th></tr>");
+            out.println("<table style=\"border-collapse: collapse;\">");
+            out.println("<tr style=\"border: 3px solid black;\">><th>ID</th><th>Input</th><th>Output</th><th>Cypher</th><th>Timestamp</th></tr>");
 
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -37,22 +43,24 @@ public class HelloServlet extends HttpServlet {
                 Timestamp timestamp = resultSet.getTimestamp("timestamp");
 
                 out.println("<tr>");
-                out.println("<td>" + id + "</td>");
-                out.println("<td>" + input + "</td>");
-                out.println("<td>" + output + "</td>");
-                out.println("<td>" + cypher + "</td>");
-                out.println("<td>" + timestamp + "</td>");
+                out.println("<td style=\"border: 1px solid black; padding: 5px;\">" + id + "</td>");
+                out.println("<td style=\"border: 1px solid black; padding: 5px;\">" + input + "</td>");
+                out.println("<td style=\"border: 1px solid black; padding: 5px;\">" + output + "</td>");
+                out.println("<td style=\"border: 1px solid black; padding: 5px;\">" + cypher + "</td>");
+                out.println("<td style=\"border: 1px solid black; padding: 5px;\">" + timestamp + "</td>");
                 out.println("</tr>");
             }
             out.println("</table>");
             out.println("</body></html>");
         } catch (SQLException e) {
             throw new ServletException("Database connection failed",e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
         }
     }
-
         public void destroy() {
-    }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
 }
